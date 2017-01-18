5c2a76802db1638df0ab8f1fe4c115cc444d3605
Rebelpebble <neasbrian@gmail.com>
2017-01-18 00:40:00 -0600

    Add classes for monsters, weapons, and armor

---
+++ mountain_quest.py
@@ -0,0 +1,47 @@
+
+### Monsters ###
+class Monster(object):
+    pass
+
+class Goblin(Monster):
+    pass
+
+class Troll(Monster):
+    pass
+
+class Basilisk(Monster):
+    pass
+
+class Dragon(Monster):
+    pass
+######
+
+### Weapons ###
+class Weapon(object):
+    pass
+
+class Hands(Weapon):
+    pass
+
+class IronSword(Weapon):
+    pass
+
+class SteelSword(Weapon):
+    pass
+
+class DragonSword(Weapon):
+    pass
+######
+
+### Armor ###
+class Armor(object):
+    pass
+
+class Clothes(Armor):
+    pass
+
+class IronArmor(Armor):
+    pass
+
+class SteelArmor(Armor):
+    pass
