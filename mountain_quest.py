5165d9ec2165f70c4ac97cccce6903225b42c02b
Rebelpebble <neasbrian@gmail.com>
2017-01-18 00:52:31 -0600

    Add display names, starting health, and attack sequences for each monster

--- mountain_quest.py
+++ mountain_quest.py
@@ -1,19 +1,45 @@
+import random
 
 ### Monsters ###
 class Monster(object):
-    pass
+
+    def __init__(self):
+        # TREY: Why is it necessary to do this?
+        self.current_health = self.starting_health
+
+    def is_alive(self):
+        return current_health > 0
+
+    def take_damage(self, damage):
+        self.current_health = self.current_health - damage
 
 class Goblin(Monster):
-    pass
+    display_name = "goblin"
+    starting_health = 10
+
+    def attack(self):
+        return random.randint(0, 5)
 
 class Troll(Monster):
-    pass
+    display_name = "troll"
+    starting_health = 20
+
+    def attack(self):
+        return random.randint(2, 10)
 
 class Basilisk(Monster):
-    pass
+    display_name = "basilisk"
+    starting_health = 30
+
+    def attack(self):
+        return random.randint(9, 15)
 
 class Dragon(Monster):
-    pass
+    display_name = "grand dragon"
+    starting_health = 100
+
+    def attack(self):
+        return random.randint(30, 50)
 ######
 
 ### Weapons ###
@@ -45,3 +71,4 @@ class IronArmor(Armor):
 
 class SteelArmor(Armor):
     pass
+######
