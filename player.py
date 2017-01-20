c1af8c002c9c599d4c94ff4eab654ee07f352710
Rebelpebble <neasbrian@gmail.com>
2017-01-19 19:18:08 -0600

    Remove "my_" from player class variables and add gold increase and decrease methods

--- player.py
+++ player.py
@@ -1,13 +1,19 @@
 class Player(object):
     starting_health = 20
 
-    def __init__(self, my_weapon, my_armor, quests, inventory, gold):
-        self.my_health = self.starting_health
-        self.my_weapon = my_weapon
-        self.my_armor = my_armor
+    def __init__(self, weapon, armor, quests, inventory, gold):
+        self.health = self.starting_health
+        self.weapon = my_weapon
+        self.armor = my_armor
         self.quests = quests
         self.inventory = inventory
         self.gold = gold
 
     def reset_health(self):
         self.my_health = self.starting_health
+
+    def add_gold(self, gold_gained):
+        self.gold += gold_gained
+
+    def subtract_gold(self, gold_removed):
+        self.gold += gold_removed
