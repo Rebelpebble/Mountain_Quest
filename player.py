1c8cfea48ed508a4f57e89bc82754fb35d2cf39d
Rebelpebble <neasbrian@gmail.com>
2017-01-19 19:19:36 -0600

    Remove "my_"

--- player.py
+++ player.py
@@ -3,14 +3,14 @@ class Player(object):
 
     def __init__(self, weapon, armor, quests, inventory, gold):
         self.health = self.starting_health
-        self.weapon = my_weapon
-        self.armor = my_armor
+        self.weapon = weapon
+        self.armor = armor
         self.quests = quests
         self.inventory = inventory
         self.gold = gold
 
     def reset_health(self):
-        self.my_health = self.starting_health
+        self.health = self.starting_health
 
     def add_gold(self, gold_gained):
         self.gold += gold_gained
